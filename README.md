14666e6b514c61ff40f771b7207cc2854b2c1950
Oink70 <oink1970@hotmail.com>
2021-12-01 10:10:46 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# Verus-CLI-upgrader
-One script to install/update your Verus Daemon to the latest release available in the official Verus Github Repo
+# Verus-CLI-tools
+
+
+##Content
+ - `auto-verus.sh`
+
+### auto-verus.sh
+##### Description
+1) if no `verusd` binary is found in the path or local folder:
+ - Download the latest official version from the VerusCoin Github repository, based on OS and processor architecture (yes, it works on ARM-linux as well).
+ - Check the download using SHA256.
+ - Call the `fetch-params` script from the downloaded release (downloads *Zcash parameters* to the required location).
+ - Call the `fetch-bootstrap` script from the downloaded release (downloads, verifies and extracts *bootstrap archive*).
+ - Start the Verus wallet (CLI).
+2) if `verusd` binaries are found, but not running:
+ - download the latest official version from the VerusCoin Github repository, based on OS and processor architecture.
+ - Check the download using SHA256.
+ - Rename the existing binaries to `*-old`.
+ - Start the Verus wallet (CLI).
+3) if `verusd` is found running and the local chain is not forked:
+ - download the latest official version from the VerusCoin Github repository, based on OS and processor architecture.
+ - Check the download using Verus signatures.
+ - Rename the existing binaries with the suffix of the current running version.
+ - Start the Verus wallet (CLI).
+4) if `verusd` is found running, but the built-in checks determine the chain is forked:
+ - download the latest official version from the VerusCoin Github repository, based on OS and processor architecture.
+ - Check the download using SHA256.
+ - Rename the existing binaries to `*-old`.
+ - Call the `fetch-bootstrap` script from the downloaded release (downloads, verifies and extracts *bootstrap archive*).
+ - start the Verus wallet (CLI) with `-zapwallettxes=2 -rescan` options
+##### Prerequisites
+ - Linux OS
+ - `curl` and `jq` installed
+##### Usage
+ - Execute `auto-verus.sh`. Command line parameters are ignored.
+
+
+## DISCLAIMER
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notices and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ auto-verus.sh
@@ -0,0 +1,271 @@
+#!/bin/bash
+
+## © Oink 2021, released under MIT license
+##
+## Required binaries:
+## jq, curl
+
+## This script will install or upgrade the latest released Verus-CLI binaries from
+## the official VerusCoin Github repository (https://github.com/VerusCoin/VerusCoin)
+
+## Determine current path
+SCRIPT_PATH=$(dirname $(realpath $0))
+
+## determine if the needed dependencies are available
+# --curl--
+if ! command -v curl &>/dev/null ; then
+    echo "curl not found. Please install using your package manager."
+    exit 1
+else
+    CURL=$(which curl)
+fi
+
+# --jq--
+if ! command -v jq &>/dev/null ; then
+    echo "jq not found. Please install using your package manager."
+    exit 1
+else
+    JQ=$(which jq)
+fi
+
+# --verus-- (if multiple  instances are found, the first is selected)
+if ! command -v verus &>/dev/null ; then
+    if ! command -v ./verus &>/dev/null ; then
+                VERUS_BINARY=0
+        else
+                VERUS_BINARY=1
+                VERUS=$SCRIPT_PATH/verus
+        fi
+else
+    VERUS=$(which verus)
+        VERUS_BINARY=1
+fi
+
+# --verusd-- (if multiple  instances are found, the first is selected)
+if ! command -v verusd &>/dev/null ; then
+    if ! command -v ./verusd &>/dev/null ; then
+                VERUSD_BINARY=0
+        else
+                VERUSD_BINARY=1
+                VERUSD=$SCRIPT_PATH/verusd
+        fi
+else
+    VERUSD=$(which verusd)
+        VERUSD_BINARY=1
+fi
+
+## Check if the daemon is running.
+DAEMON_ACTIVE=0
+if [[ "${VERUS_BINARY}" == "1" ]]; then
+	count=$(${VERUS} getconnectioncount 2>/dev/null)
+	case $count in
+		''|*[!0-9]*) DAEMON_ACTIVE=0 ;;
+		*) DAEMON_ACTIVE=1 ;;
+	esac
+fi
+
+## determine processor Architecture
+lscpu | grep "Architecture" | grep "aarch64"
+lscpu | grep "Architecture" | grep "x68_64"
+
+## determine OS
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    OS="MacOS"
+	## Remove next 3 lines if tested successfully on MacOS
+	echo "${OS} is not tested yet."
+	echo "Exiting..."
+	exit 0
+else
+    OS="Linux"
+	if [[ "$(lscpu | grep "Architecture")" == *"aarch64"* ]]; then
+		ARCHITECTURE="arm64"
+	else
+		ARCHITECTURE="x86_64"
+	fi
+fi
+
+## determine latest Github version
+echo "Determining last version on GitHub..."
+GITHUB_RELEASE_JSON=$(${CURL} --silent "https://api.github.com/repos/VerusCoin/VerusCoin/releases?per_page=1")
+GITHUB_LATEST_RELEASE=$(echo $GITHUB_RELEASE_JSON | ${JQ} -r .[0].name)
+GITHUB_DOWNLOAD_URL=$(echo $GITHUB_RELEASE_JSON | ${JQ} ".[0].assets | .[] | select ( .browser_download_url | contains (\"${OS}\"))" | ${JQ} -r "select (.browser_download_url | contains (\"${ARCHITECTURE}\")) | .browser_download_url")
+GITHUB_DOWNLOAD_NAME=$(echo $GITHUB_RELEASE_JSON | ${JQ} ".[0].assets | .[] | select ( .name | contains (\"${OS}\"))" | ${JQ} -r "select (.browser_download_url | contains (\"${ARCHITECTURE}\")) | .name")
+
+## check version of running daemon
+echo "Checking if daemon is running..."
+if [[ "${DAEMON_ACTIVE}" == "1" ]]; then
+	CURRENT_VERSION=$(${VERUS} getinfo | ${JQ} -r .VRSCversion)
+
+	## compare running version (if any) to github version
+	if [ ${GITHUB_LATEST_RELEASE} = v${CURRENT_VERSION} ]; then
+        	echo "You are already running ${GITHUB_LATEST_RELEASE}"
+        	echo "No further actions are needed."
+        	echo "Exiting now..."
+        	sleep 3s
+        	exit 0
+	fi
+
+	## Check if the node is forked, exit if forking is detected
+	echo "Checking if chain is forked..."
+	CHECK_FORK="UNKNOWN"
+	while [[ ! "$CHECK_FORK" == "OK" ]]; do
+		# collect data
+		HASH_LOCAL=$(${VERUS} getbestblockhash)
+		HEIGHT_LOCAL=$(${VERUS} getblock ${HASH_LOCAL} 1 | jq -r .height)
+		HASH_REMOTE=$(${CURL} --silent "https://explorer.verus.io/api/getblockhash?index=${HEIGHT_LOCAL}" | ${JQ} -r .)
+		# determine status
+		# either output empty = unknown
+		if [ -z "${HASH_LOCAL}" ] || [ -z "${HASH_REMOTE}" ]; then
+			CHECK_FORK="UNKNOWN"
+			echo "Chain state could not be determined. Using local chaun."
+		# equal output = OK
+		elif [ "${HASH_LOCAL}" == "${HASH_REMOTE}" ]; then
+			CHECK_FORK="OK"
+			echo "The chain is not on a fork."
+		# nonequal output = critical
+		else
+			CHECK_FORK="CRIT"
+			echo "The chain seems to be forked. Bootstrap will be activated."
+			break
+		fi
+		sleep 5s
+	done
+else
+	CHECK_FORK="UNKNOWN"
+	CURRENT_VERSION=unknown
+fi
+
+## Download the latest release binary for this system
+echo "Downloading latest version..."
+${CURL} --silent --output "${GITHUB_DOWNLOAD_NAME}" -# -L -C - "${GITHUB_DOWNLOAD_URL}"
+
+## Unpack the downloaded archive and get the signature data.
+echo "Extracting downloaded version..."
+FILELIST=$(tar -xvf "${GITHUB_DOWNLOAD_NAME}")
+for i in $FILELIST; do
+  if [[ $i = *signature* ]]; then
+    SIGNATURE_FILE=$i
+  else
+    SIGNED_BINARY=$i
+  fi
+done
+
+printf "Verifying downloaded version"
+SIGNATURE_JSON=$(cat "${SIGNATURE_FILE}")
+SIGNATURE=$(echo "${SIGNATURE_JSON}" | ${JQ} '.signature')
+SIGNER=$(echo "${SIGNATURE_JSON}" | ${JQ} '.signer')
+SHA256=$(echo "${SIGNATURE_JSON}" | ${JQ} -r '.hash')
+
+## Verify the signature or sha256, depending on if chain is running and not forked or not running.
+if [[ ( "${DAEMON_ACTIVE}" == "0" ) || ( "${CHECK_FORK}" == "CRIT" ) ]]; then
+  printf ", using SHA256 checksum method...\n"
+  if [[ $(shasum -a256 ${SIGNED_BINARY}) == ${SHA256}* ]]; then
+    CHECK_RESULT=true
+  else
+    CHECK_RESULT=false
+  fi
+else
+  echo ", using the VerusID signature..."
+  CHECK_RESULT=$(bash -c "${VERUS} verifyfile ${SIGNER} ${SIGNATURE} ${SCRIPT_PATH}/${SIGNED_BINARY}")
+fi
+
+if [[ "${CHECK_RESULT}" == "true" ]]; then
+        echo "Download verified: OK"
+	if [[ -d verus-cli ]]; then
+		rm -rf verus-cli
+	fi
+	tar -xf "${SIGNED_BINARY}"
+	rm -f ${GITHUB_DOWNLOAD_NAME} ${FILELIST}
+else
+	echo "The integrity check of ${SIGNED_BINARY} failed."
+	echo "Removing downloaded files..."
+	rm -f ${GITHUB_DOWNLOAD_NAME} ${FILELIST}
+	echo "The upgrade procedure is stopped for security reasons."
+	echo "Exiting now..."
+	sleep 3s
+	exit 1
+fi
+
+
+## Rename daemon and RCP-client, new name depending on conditions.
+if [[ "${DAEMON_ACTIVE}" == "1" ]]; then
+	mv ${VERUSD} ${VERUSD}-v${CURRENT_VERSION}
+	mv ${VERUS} ${VERUS}-v${CURRENT_VERSION}
+	cp ${SCRIPT_PATH}/verus-cli/verusd ${VERUSD}
+	cp ${SCRIPT_PATH}/verus-cli/verus ${VERUS}
+	NEW_INSTALL=false
+elif [[ "${VERUS_BINARY}" == "1" ]]; then
+	mv ${VERUSD} ${VERUSD}-old
+	mv ${VERUS} ${VERUS}-old
+        cp ${SCRIPT_PATH}/verus-cli/verusd ${VERUSD}
+        cp ${SCRIPT_PATH}/verus-cli/verus ${VERUS}
+	NEW_INSTALL=false
+else
+        ## ask if this is a virgin install
+        read -t 30 -p "Is this a new installation of Verus? [Y/n] " FEEDBACK
+        case ${FEEDBACK} in
+                '')
+                        NEW_INSTALL=true
+                        printf "\n"
+                        ;;
+                [yY][eE][sS]|[yY])
+                        NEW_INSTALL=true
+                        ;;
+                *)
+                        NEW_INSTALL=false
+                        ;;
+        esac
+fi
+
+## Stop active daemon
+if [[ "${DAEMON_ACTIVE}" == "1" ]]; then
+        ${VERUS} stop
+  ## Monitoring if the daemon is still running, before continuing
+  while ps -u "${USER}" x | grep "${VERUSD} " | grep -v "grep"; do
+    sleep 2s
+  done
+fi
+
+## in case of a new install, download zcashparams
+if [[ "${NEW_INSTALL}" == "true" ]]; then
+		source verus-cli/fetch-params
+fi
+
+## in case of a new install or a forked chain, bootstrap the wallet.
+if [[ ("${NEW_INSTALL}" == "true") || ("${CHECK_FORK}" == "CRIT") ]]; then
+		source verus-cli/fetch-bootstrap
+fi
+
+## start daemon normally, except for forked chain.
+## Forked chain will start with `-zapwallettxes=2 -rescan`
+if [[ "${DAEMON_ACTIVE}" == "1" ]]; then
+	if [[ "${CHECK_FORK}" == "CRIT" ]]; then
+		cd ~/.komodo/VRSC
+		${VERUSD} -zapwallettxes=2 -rescan -daemon 1>/dev/null 2>&1 &
+	else
+		cd ~/.komodo/VRSC
+		${VERUSD} -daemon 1>/dev/null 2>&1 &
+	fi
+else
+	cp ${SCRIPT_PATH}/verus-cli/verusd .
+	cp ${SCRIPT_PATH}/verus-cli/verus .
+	cd ~/.komodo/VRSC
+	${SCRIPT_PATH}/verusd -daemon 1>/dev/null 2>&1 &
+fi
+
+if [[ "${NEW_INSTALL}" == "true" ]]; then
+	echo "Verus ${GITHUB_LATEST_RELEASE} is installed."
+	echo "Required Zcash parameters have been downloaded."
+	echo "Bootstrap archive has been downloaded and extracted."
+	echo "Verus daemon is starting up now."
+else
+	echo "Verus is upgraded to ${GITHUB_LATEST_RELEASE}."
+	if [[ "${CHECK_FORK}" == "CRIT" ]]; then
+		echo "Your chain was forked."
+		echo "Bootstrap archive has been downloaded and extracted."
+		echo "your wallet will be rescanned."
+	fi
+	echo "Verus daemon is starting up now."
+fi
+
+#EOF
